fdabbbf1f9a409fe85849242a28d7b342d109dd6
André Bernardo <andre.vbernardo97@gmail.com>
2020-07-13 21:45:35 -0300

    criação do teclado virtual

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import dlib
 from math import hypot
 olho_direito = list(range(36, 42))
 olho_esquerdo = list(range(42, 48))
+sobrancelha_esquerda = list(range(18,22))
+sobrancelha_direita = list(range(23,27))
 cap = cv2.VideoCapture(0)
 
 detector = dlib.get_frontal_face_detector() #função da biblioteca dlib para detectar o rosto
@@ -19,12 +21,51 @@ def getPiscada(pontos,landmarks):
     razao = (tamanhoH/tamanhoV)
     
     return razao
-    
+        
 def getTamanhoLinhasOlho(p1,p2,p3,p4):
     tamanhoLinhaV = hypot((p3[0] - p4[0]),(p3[1] - p4[1]))
     tamanhoLinhaH = hypot((p1[0] - p2[0]),(p1[1] - p2[1]))
     
     return tamanhoLinhaV,tamanhoLinhaH
+
+def getRazaoOlho(pontos,landmarks):
+        olho_esquerdo_contorno = np.array([(landmarks.part(pontos[0]).x,landmarks.part(pontos[0]).y),
+                                  (landmarks.part(pontos[1]).x,landmarks.part(pontos[1]).y),
+                                  (landmarks.part(pontos[2]).x,landmarks.part(pontos[2]).y),
+                                  (landmarks.part(pontos[3]).x,landmarks.part(pontos[3]).y),
+                                  (landmarks.part(pontos[4]).x,landmarks.part(pontos[4]).y),
+                                  (landmarks.part(pontos[5]).x,landmarks.part(pontos[5]).y)],np.int32)
+        #cv2.polylines(quadro,[olho_esquerdo_contorno],True,(0,0,255),2) #desenhado um contorno no olho
+        
+        altura,largura,_ = quadro.shape
+        mascara = np.zeros((altura,largura),np.uint8)
+        cv2.polylines(mascara,[olho_esquerdo_contorno],True,255,2)
+        cv2.fillPoly(mascara,[olho_esquerdo_contorno],255)
+        olho = cv2.bitwise_and(quadro_cinza,quadro_cinza,mask=mascara)
+        min_x = np.min(olho_esquerdo_contorno[:,0])
+        max_x = np.max(olho_esquerdo_contorno[:,0])
+        min_y = np.min(olho_esquerdo_contorno[:,1])
+        max_y = np.max(olho_esquerdo_contorno[:,1])
+        
+        olho_cinza = olho[min_y:max_y,min_x:max_x]
+        #olho_gray = cv2.cvtColor(olho,cv2.COLOR_BGR2GRAY) #imagem do olho convertida para escala de cinza para melhor precisão
+        olho = cv2.resize(olho_cinza,None,fx=5,fy=5)
+        
+        _,threshold_olho = cv2.threshold(olho_cinza,80,255,cv2.THRESH_BINARY) #feito threshold na imagem cinza para detectar mais precisamente a direção do olho
+        height, width = threshold_olho.shape
+        esquerdo_t = threshold_olho[0:height,0:int(width/2)]
+        esquerdo_branco = cv2.countNonZero(esquerdo_t)
+        direito_t = threshold_olho[0:height,int (width/2):width]
+        direito_branco = cv2.countNonZero(direito_t)
+        
+        if esquerdo_branco == 0:
+            razao_olho = 1
+        elif direito_branco == 0:
+            razao_olho = 5
+        else:
+            razao_olho = esquerdo_branco/direito_branco
+        
+        return razao_olho
 
 def getPontosOlho(landmarks,pontos):
     p1 = (landmarks.part(pontos[0]).x,landmarks.part(pontos[0]).y)
@@ -47,37 +88,25 @@ while True:
 
         piscadaEsquerdo = getPiscada([36,37,38,39,40,41],landmarks)
         piscadaDireito = getPiscada([42,43,44,45,46,47],landmarks)
+              
+        if (piscadaEsquerdo+piscadaDireito)/2 > 5:
+            cv2.putText(quadro,"PISCOU",(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2,cv2.LINE_AA,False) 
         
-        if (piscadaEsquerdo+piscadaDireito)/2 > 4:
-            cv2.putText(quadro,"PISCASTE",(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2,cv2.LINE_AA,False) 
+        razao_olhoEsquerdo = getRazaoOlho([36,37,38,39,40,41],landmarks)
+        razao_olhoDireito = getRazaoOlho([42,43,44,45,46,47],landmarks)
         
-        olho_esquerdo_contorno = np.array([(landmarks.part(36).x,landmarks.part(36).y),
-                                  (landmarks.part(37).x,landmarks.part(37).y),
-                                  (landmarks.part(38).x,landmarks.part(38).y),
-                                  (landmarks.part(39).x,landmarks.part(39).y),
-                                  (landmarks.part(40).x,landmarks.part(40).y),
-                                  (landmarks.part(41).x,landmarks.part(41).y)],np.int32)
-        #cv2.polylines(quadro,[olho_esquerdo_contorno],True,(0,0,255),2) #desenhado um contorno no olho
+        razao_olhos = (razao_olhoEsquerdo + razao_olhoDireito)/2
         
-        altura,largura,_ = quadro.shape
-        mascara = np.zeros((altura,largura),np.uint8)
-        cv2.polylines(mascara,[olho_esquerdo_contorno],True,255,2)
-        cv2.fillPoly(mascara,[olho_esquerdo_contorno],255)
-        olho_esquerdo = cv2.bitwise_and(quadro_cinza,quadro_cinza,mask=mascara)
-        min_x = np.min(olho_esquerdo_contorno[:,0])
-        max_x = np.max(olho_esquerdo_contorno[:,0])
-        min_y = np.min(olho_esquerdo_contorno[:,1])
-        max_y = np.max(olho_esquerdo_contorno[:,1])
+        if razao_olhos <= 0.7:
+            cv2.putText(quadro,"direita",(50,100),cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),3)
+            
+        elif 0.7<razao_olhos<1.5:
+            cv2.putText(quadro,"centro",(50,100),cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),3)
+        else:
+            cv2.putText(quadro,"esquerda",(50,100),cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),3)
         
-        olho_cinza = olho_esquerdo[min_y:max_y,min_x:max_x]
-        #olho_gray = cv2.cvtColor(olho,cv2.COLOR_BGR2GRAY) #imagem do olho convertida para escala de cinza para melhor precisão
-        olho = cv2.resize(olho_cinza,None,fx=5,fy=5)
+        #cv2.putText(quadro,str(razao_olhos),(50,150),cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),3)
         
-        _,threshold_olho = cv2.threshold(olho_cinza,70,255,cv2.THRESH_BINARY) #feito threshold na imagem cinza para detectar mais precisamente a direção do olho
-        threshold_olho = cv2.resize(threshold_olho,None,fx=5,fy=5)
-        cv2.imshow("olho",olho)
-        cv2.imshow("threshold",threshold_olho)
-        cv2.imshow("mascara",olho_esquerdo)
     cv2.imshow("quadro",quadro)
 
     key = cv2.waitKey(1)
---
+++ teclado.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 20:14:25 2020
+
+@author: avber
+"""
+
+
+import cv2
+import numpy as np
+
+teclado = np.zeros((1000,1500,3),np.uint8)
+
+letras = {0: "Q", 1: "W", 2: "E", 3: "R", 4: "T", 5: "Y", 6: "Y", 7: "I", 8: "O", 9: "P",
+          10: "A", 11: "S", 12: "D", 13: "F", 14: "G", 15: "H", 16: "J", 17: "K", 18: "L", 19: "C",
+          20: "Z", 21: "X", 22: "C", 23: "V", 24: "B", 25: "N", 26: "M"}
+              
+
+def criaTeclado(index,letra,selector):
+    
+    if index == 0:
+       x = 0
+       y = 0
+    elif index == 1:
+        x = 70
+        y = 0
+    elif index == 2:
+        x = 140
+        y = 0
+    elif index == 3:
+        x = 210
+        y = 0
+    elif index == 4:
+        x = 280
+        y = 0
+    elif index == 5:
+        x = 350
+        y = 0
+    elif index == 6:
+        x = 420
+        y = 0
+    elif index == 7:
+        x = 490
+        y = 0
+        
+    elif index == 8:
+        x = 560
+        y = 0
+    
+    elif index == 9:
+        x = 630
+        y = 0
+
+    elif index == 10:
+        x = 0
+        y = 70
+    
+    elif index == 11:
+        x = 70
+        y = 70
+    
+    elif index == 12:
+        x = 140
+        y = 70
+    
+    elif index == 13:
+        x = 210
+        y = 70
+    
+    elif index == 14:
+        x = 280
+        y = 70
+    
+    elif index == 15:
+        x = 350
+        y = 70
+    
+    elif index == 16:
+        x = 420
+        y = 70
+    
+    elif index == 17:
+        x = 490
+        y = 70
+        
+    elif index == 18:
+        x = 560
+        y = 70
+    
+    elif index == 19:
+        x = 630
+        y = 70
+    
+    elif index == 20:
+        x = 0
+        y = 140
+    
+    elif index == 21:
+        x = 70
+        y = 140
+    
+    elif index == 22:
+        x = 140
+        y = 140
+    
+    elif index == 23:
+        x = 210
+        y = 140
+        
+    elif index == 24:
+        x = 280
+        y = 140
+        
+    elif index == 25:
+        x = 350
+        y = 140
+        
+    elif index == 26:
+        x = 420
+        y = 140
+    
+    
+        
+    # Teclas
+
+    width = 70
+    height = 70
+    th = 2
+    
+    if selector is True:
+        cv2.rectangle(teclado, (x + th, y + th), (x + width - th, y + height - th), (255, 255, 255), -1)
+    else:
+        cv2.rectangle(teclado, (x + th, y + th), (x + width - th, y + height - th), (255, 0, 0), th)
+    
+    # Texto 
+    font_scale = 5
+    text_size = cv2.getTextSize(letra, cv2.FONT_HERSHEY_PLAIN, font_scale, 2)[0]
+    width_text, height_text = text_size[0], text_size[1]
+    text_x = int((width - width_text) / 2) + x
+    text_y = int((height + height_text) / 2) + y
+    cv2.putText(teclado, letra, (text_x, text_y), cv2.FONT_HERSHEY_PLAIN, font_scale, (255, 0, 0), 2)
+    
+for i in range(26):
+    criaTeclado(i,letras[i],False)
+    
+
+
+
+
+
+
+cv2.imshow("teclado", teclado)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
